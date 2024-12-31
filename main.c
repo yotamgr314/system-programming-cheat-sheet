964e2496041c335f457f263322edef00258bfc15
Yotam Greenstein <yotamgr314@gmail.com>
2024-12-31 19:06:32 +0200

    asdsad
    asdasd

--- main.c
+++ main.c
@@ -18,5 +18,18 @@ pthread_cleanup_push(cleanUpCallbackFunc,dynmicItem) // NOTE: register a callbac
                                                      // 02) pay attention: if the thread runs out of scope and we did not called pthread_cleanup_pop(1) then the callback clean up func wont execute ! 
 
 
-pthread_mutex_t // a data type which represents mutex. 
-pthread_mutex_t my_mutex = PTHREAD_MUTEX_INITIALIZER; // NOTE: 
+pthread_mutex_t // NOTE: a data type which represents mutex variable. 
+pthread_mutex_t my_mutex = PTHREAD_MUTEX_INITIALIZER; // NOTE: a mutex variable must be initialize with PTHREAD_MUTEX_INITIALIZER or with the pthread_mutex_init() function.
+
+pthread_mutex_lock(pthread_mutex_t *mutex); // NOTE : a way for a thread to lock a mutex. in the mutex is locked at the time pthread_mutex_lock is called the thread blocks until it is unlocked.
+pthread_mutex_unlock(pthread_mutex_t *mutex); // NOTE a way for a thread to unlock a mutex. 
+
+pthread_mutex_trylock(pthread_mutex_t* mutex); // NOTE : a way for a thread to lock a mutex asynchoarncly (without blocking).
+                                               // 01) if the mutex is unlocked at the time of calling, the mutex becomes locked and the return value is 0.
+                                               // 02) if the mutex is locked at the time of calling, the function returns instantly with a failure status of EBUSY.
+
+TERMS SECTION
+/*
+Deadlock - a situation on which a thread enters a waiting state, waiting for some resource to become available, for indefinite amount of time. 
+
+*/
